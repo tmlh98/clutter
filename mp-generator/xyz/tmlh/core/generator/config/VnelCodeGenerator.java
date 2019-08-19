8bf99748b2922dda3659edba113428dc89358271
TianXin <tmlh98@aliyun.com>
2019-08-19 11:52:22 +0800

    mp generator

---
+++ mp-generator/xyz/tmlh/core/generator/config/VnelCodeGenerator.java
@@ -0,0 +1,39 @@
+package xyz.tmlh.core.generator.config;
+
+/**
+ * <p>
+ * Title: VnelCodeGenerator
+ * </p>
+ * <p>
+ * Description: 定义配置常量
+ * </p>
+ * 
+ * @author TianXin
+ * @created 2019年2月25日 下午9:18:28
+ * @modified [who date description]
+ * @check [who date description]
+ */
+public interface VnelCodeGenerator {
+
+    String DRIVER_NAME = "tmlh.security.generator.driverName";
+    String USERNAME = "tmlh.security.generator.username";
+    String PASSWORD = "tmlh.security.generator.password";
+    String URL = "tmlh.security.generator.url";
+    String AUTHOR = "tmlh.security.generator.author";
+    String PAGE_PARENT = "tmlh.security.generator.pageParent";
+    String MODULE_NAME = "tmlh.security.generator.moduleName";
+    String SUPER_ENTITY_CLASS = "tmlh.security.generator.superEntityClass";
+    String SUPER_CONTROLLER_CLASS = "tmlh.security.generator.superControllerClass";
+    String TABLE_PREFIX = "tmlh.security.generator.tablePrefix";
+    String INCLUDE_TABLE_NAME = "tmlh.security.generator.includeTableName";
+    String EXCLUDE_TABLE_NAME = "tmlh.security.generator.excludeTableName";
+    
+    String PAGE_ENTITY = "tmlh.security.generator.pageEntity";
+    String PAGE_SERVICE = "tmlh.security.generator.pageService";
+    String PAGE_SERVICE_IMPL = "tmlh.security.generator.pageServiceImpl";
+    String PAGE_CONTROLLER = "tmlh.security.generator.pageController";
+    String PAGE_MAPPER = "tmlh.security.generator.pageMapper";
+    
+    String ROOT_PATH = "tmlh.security.generator.rootPath";
+    
+}
